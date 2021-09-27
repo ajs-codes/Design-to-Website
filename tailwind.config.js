b0dc381c240aaa70757d098eb1ee1e230c364276
ajs_codes <sharanajs2001@gmail.com>
2021-09-27 13:50:47 +0530

    front page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0078E7",
+        secondary: "#797979"
+      },
+      fontFamily: {
+        body: ["Poppins"]
+      }
+    },
   },
   variants: {
     extend: {},
